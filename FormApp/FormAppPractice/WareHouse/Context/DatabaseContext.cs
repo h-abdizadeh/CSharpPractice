9b2dae55fb84302de44706e7115e361e5d79d181
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-03-18 17:23:15 +0330

    Add files via upload

--- FormApp/FormAppPractice/WareHouse/Context/DatabaseContext.cs
+++ FormApp/FormAppPractice/WareHouse/Context/DatabaseContext.cs
@@ -1,7 +1,33 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using WareHouse.Models;
 
 namespace WareHouse.Context;
 
-public class DatabaseContext//:Dbcontext
+public class DatabaseContext : DbContext
 {
+    //model -> table 
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    //database config
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        //base.OnConfiguring(options);
+        options.UseSqlite($"data source={Application.StartupPath}\\WareHouseApp.db");
+    }
+
+
+    //default values (seed)
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //base.OnModelCreating(builder);
+        var defaultGroup = new List<Group>()
+        {
+            new Group(){Id=1,Title="لوازم خانگی"},
+            new Group(){Id=2,Title="محصولات بهداشتی"},
+            new Group(){Id=3,Title="مواد خوراکی"}
+        };
+
+        builder.Entity<Group>().HasData(defaultGroup);
+    }
 }
---
+++ FormApp/FormAppPractice/WareHouse/Migrations/20240317164604_createDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WareHouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class createDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Img = table.Column<string>(type: "TEXT", nullable: true),
+                    Inventory = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Groups",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "لوازم خانگی" },
+                    { 2, "محصولات بهداشتی" },
+                    { 3, "مواد خوراکی" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_GroupId",
+                table: "Products",
+                column: "GroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
