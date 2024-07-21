66ab502159acba8a813b2255098ee9b19306e988
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-07-21 15:53:54 +0330

    Update CSharp04.cs

--- CSharp04.cs
+++ CSharp04.cs
@@ -138,14 +138,172 @@ using static System.Console;
 //}
 
 //13
-Write("enter number : ");
-int number = int.Parse(ReadLine());
+//Write("enter number : ");
+//int number = int.Parse(ReadLine());
 
-if (number % 2 == 0)
-{
-    WriteLine("even");
-}
-else
-{
-    WriteLine("odd");
-}
+//if (number % 2 == 0)
+//{
+//    WriteLine("even");
+//}
+//else
+//{
+//    WriteLine("odd");
+//}
+
+//14
+//Write("enter number 1 : ");
+//var n=float.Parse(ReadLine());
+
+//Write("enter number 2 : ");
+//var m=float.Parse(ReadLine());
+
+//if (n >= 16)
+//{
+//    var sum = n + m;
+//    WriteLine(sum/2);
+//}
+//else
+//{
+//    WriteLine(n);
+//}
+
+//15
+//Write("enter number 1 : ");
+//var n = float.Parse(ReadLine());
+
+//Write("enter number 2 : ");
+//var m = float.Parse(ReadLine());
+
+//if (n > m)
+//    Write(n);
+//else
+//    Write(m);
+
+//16
+//Write("enter character : ");
+//var ch = ReadKey();
+//WriteLine();
+////WriteLine(ch);
+////WriteLine(ch.Key);
+////WriteLine(ch.KeyChar);
+//WriteLine((int)ch.KeyChar);
+
+////var newCh =
+////(int)ch.KeyChar > 100 ? (int)ch.KeyChar - 25 : (int)ch.KeyChar;
+
+//var ascii = (int)ch.KeyChar;
+//var newCh = ascii > 100 ? ascii - 25 : ascii;
+
+//WriteLine(newCh);
+//WriteLine((char)newCh);
+
+//17
+//Write("enter light color : ");
+//string color = ReadLine().ToLower();//RED >>>>>> red
+
+//WriteLine(color);
+
+//if(color is "red")//==
+//{
+//    Write("STOP");
+//}
+//else if(color is "yellow")
+//{
+//    Write("SLOW");
+//}
+//else if(color is "green")
+//{
+//    Write("GO");
+//}
+//else
+//{
+//    Write("ALARM");
+//    Beep(300, 1000);
+//}
+
+//18
+//Write("enter number 1 : ");
+//var a = int.Parse(ReadLine());
+
+//Write("enter number 2 : ");
+//var b = int.Parse(ReadLine());
+
+//Write("enter number 3 : ");
+//var c = int.Parse(ReadLine());
+
+////1
+////if (a > b)
+////    if (a > c) Write(a);
+////    else Write(c);
+////else
+////    if (b > c) Write(b);
+////    else  Write(c);
+
+////2 - and
+//if (a > b && a > c) Write(a);
+//else if (b > a && b > c) Write(b);
+//else Write(c);
+
+//19
+//Write("enter number 1 : ");
+//var a = float.Parse(ReadLine());
+
+//Write("enter number 2 : ");
+//var b = float.Parse(ReadLine());
+
+//if (a >= 15 || b >= 15)
+//    Write(a + b);
+////else
+////    Write("failed");
+
+
+//20
+////1
+////Write("enter number : ");
+//////var n = int.Parse(ReadLine());
+////var n = double.Parse(ReadLine());
+
+////Write(n * n);
+
+////2 - try
+//try
+//{
+//    Write("enter number : ");
+//    //var n = int.Parse(ReadLine());
+//    var n = double.Parse(ReadLine());
+
+//    Write(n * n);
+//}
+////catch
+//catch (Exception error)
+//{
+//    //throw;
+//    //Write("ERROR");
+//    //Write(error);
+//    ForegroundColor = ConsoleColor.Red;
+//    Write(error.Message);
+//}
+//BackgroundColor=ConsoleColor.White;
+////Clear();
+//ForegroundColor = ConsoleColor.Black;
+
+//21
+//using static System.String;
+
+//int[] numbers1 = new int[5];
+//int[] numbers2 = { 5, 7, -11, 8, 3 };
+//var numbers3 = new double[5];
+////var numbers4 = new int[] { 5, 8, 9, -5, 4 };
+//var names1 = new string[5];
+//string[] names2 = 
+//    { "ali", "amir", "sara", "mahdi", "narges" };
+
+//WriteLine(numbers1[3]);
+//WriteLine(numbers2[1]);
+//WriteLine(names1[4]);
+//WriteLine(names2[0]);
+//WriteLine(numbers2);
+//WriteLine(Join(',',numbers2));
+//WriteLine(Join('-',names2));
+//WriteLine(Join(',',numbers1));
+//WriteLine(Join(',',names1));
